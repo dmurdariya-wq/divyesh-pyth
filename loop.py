0d5ec91e4072f8976a90fd59f3d4fdd4079974d5
dmurdariya-wq <dmurdariya@gmail.com>
2026-02-16 11:26:42 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,32 @@
+# 1 1 to 3
+for i in range(5):
+  print(i)
+
+#2 hello print 
+for i in range(3):
+  print("hello")
+
+#3 1 to 10
+for i in range(1,11):
+  print(i)
+  
+#4 even number 
+for number in range(1, 16): 
+    if number % 2 == 0:     
+        print(number)   
+        
+            
+#5 table 5
+for i in range(1, 11):
+     print("5 x",i,"=",5*1)
+     
+#6 list
+numbers = [10, 24, 76, 23, 12]     
+print(number)
+
+#7 total
+numbers = [10, 20, 30, 40, 50]
+total = 0
+for num in numbers:
+    total += num  
+print(total)

699f428e18a6cf62c1742b32684b4eeb07aa14b9
dmurdariya-wq <dmurdariya@gmail.com>
2026-02-23 11:12:30 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,18 @@
+#1 1 to 5
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+#2 1 to  10
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+  
+#3
+num=int("enter number")
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1

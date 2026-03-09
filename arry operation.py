4b607060705649d522c7dff12efde49bc499c5a8
dmurdariya-wq <dmurdariya@gmail.com>
2026-03-09 11:32:44 +0530

    Add files via upload

---
+++ arry operation.py
@@ -0,0 +1,38 @@
+#1 length
+import array
+numbers = [10, 20, 30, 40, 50, 60]
+print(len(numbers))
+
+#2 arr = [1, 2, 3]
+arr = arr.append('i',[10,20,30,40])   
+arr . append (40)
+print(arr)
+
+#3 insert 
+arr = arr.append('i',[10,20,30,40])   
+arr . insert (30,40)
+print(arr)
+
+#4 remove
+arr = arr('i',[10,20,30,40])   
+arr . remove (30)
+print(arr)
+
+#5 pop(remove)
+arr = arr.append('i',[10,20,30,40])   
+x = arr.pop ()
+print ("removed",x)
+print(x)
+
+#6 index
+arr = arr('i',[10,20,30,40])   
+print(arr.index (30))
+
+#7 count 
+arr = arr('i',[10,20,30,40])   
+print(arr.count (20))
+
+#8 reverse 
+arr = arr('i',[10,20,30,40])   
+arr. reverse ()
+print(arr)

58276712a628cea237c416b1f827ee9cdc05940b
dmurdariya-wq <dmurdariya@gmail.com>
2026-03-11 08:07:50 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,18 @@
+#1 positive indexing
+arr = [10, "10", "20", "30", "40", "50"] 
+print(arr[0])
+print(arr[2])
+print(arr[4]) 
+
+#2 negative
+arr = [10, "10", "20", "30", "40", "50"] 
+print(arr[-1])
+print(arr[-3])
+print(arr[-5]) 
+
+#3 modify 
+from array import array
+my_array = array('i', [1, 2, 3, 4])
+my_array[0] = 5
+print(my_array)
+
---
+++ ios.py
@@ -0,0 +1,19 @@
+#1
+a="welcome"
+print(a)
+
+b="hello"
+print(b)
+
+#2
+number=int(input("enter number"))
+print(number)
+
+#3 float
+number=float(input("enter number"))
+print(number)
+
+#4 bool
+char=name(input("enter name"))
+print(char)
+
